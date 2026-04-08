849fbbacfd2a28f6aa68ac9e18b3c7f5c04b52a4
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2026-04-08 09:51:18 +0800

    Initialize eureka-server project with Gradle setup, including essential configuration files, application properties, and a basic Spring Boot application structure.

---
+++ eureka-server/src/main/java/my/test/eureka/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package my.test.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
