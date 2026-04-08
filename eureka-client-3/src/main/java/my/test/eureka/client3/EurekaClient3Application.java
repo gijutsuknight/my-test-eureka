df8c41634e4d260538bc8d539e5f5eb2866d89d1
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2026-04-08 10:32:16 +0800

    Initialize eureka-client projects (1, 2, and 3) with Gradle setup, including configuration files, application properties, and basic Spring Boot application structure for each client.

---
+++ eureka-client-3/src/main/java/my/test/eureka/client3/EurekaClient3Application.java
@@ -0,0 +1,13 @@
+package my.test.eureka.client3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClient3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClient3Application.class, args);
+	}
+
+}
---
+++ eureka-client-3/src/test/java/my/test/eureka/client3/EurekaClient3ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.test.eureka.client3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
